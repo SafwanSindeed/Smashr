10c95d839d6fb3d9d6d6b805fe0c126731963fea
Safwan Sindeed <sisindeed16@gmail.com>
2026-02-25 08:40:25 -0500

    Delete src directory

--- src/services/friendService.js
+++
@@ -1,8 +0,0 @@
-import { db } from '../lib/firebase';
-import { doc, deleteDoc } from 'firebase/firestore';
-
-// Function to remove a friend connection from the database
-export const removeFriendService = async (relationshipId) => {
-  // Delete the document that links these two friends
-  await deleteDoc(doc(db, "friends", relationshipId));
-};
