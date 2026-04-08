66e6aebb87e77a18318e571d3c133e2a45eafa6a
Ziyuan Fan <ziyuanfan0531@gmail.com>
2026-04-08 00:00:49 -0400

    add firebase config for smashr

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -1,25 +1,17 @@
-// services/firebaseConfig.js
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import Constants from "expo-constants";
-
-const extra = Constants.expoConfig?.extra || {};
 
 const firebaseConfig = {
-  apiKey: extra.FIREBASE_API_KEY,
-  authDomain: extra.FIREBASE_AUTH_DOMAIN,
-  projectId: extra.FIREBASE_PROJECT_ID,
-  storageBucket: extra.SB,
-  messagingSenderId: extra.MSG_ID,
-  appId: extra.APP_ID,
-  // measurementId removed (web analytics only)
+  apiKey: "AIzaSyDDql5XY_UOm20Al_yTCrLn6u4XrAYY9EE",
+  authDomain: "smashr-55708.firebaseapp.com",
+  projectId: "smashr-55708",
+  storageBucket: "smashr-55708.firebasestorage.app",
+  messagingSenderId: "459706072092",
+  appId: "1:459706072092:web:af78a7382ea2f15b5333d9",
 };
 
-// ✅ Prevent "Firebase App named '[DEFAULT]' already exists"
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-// ✅ Export Auth for your screens to use
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-console.log("EXTRA:", extra);
