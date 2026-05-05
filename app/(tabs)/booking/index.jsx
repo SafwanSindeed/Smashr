24818d3185884b1c0d884d4387d900c03e8fd1dd
SafwanSindeed <sisindeed16@gmail.com>
2026-05-05 12:05:32 -0400

    Add missing colors import to booking page

--- app/(tabs)/booking/index.jsx
+++ app/(tabs)/booking/index.jsx
@@ -13,6 +13,7 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
 import { useBookings } from "../../../hooks/useBooking";
+import { colors } from "../../../constants/colors";
 import { auth } from "../../../services/firebaseConfig";
 
 const TABS = [
